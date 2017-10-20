b4b98edef95e3b3689c72825cbb60d83f4fe1679
Ivan Orlov <ivan.orlov@zervant.com>
2017-10-20 09:41:29 +0200

    example

--- examples/dynamic_import_remote_js/src/index.ts
+++ examples/dynamic_import_remote_js/src/index.ts
@@ -1,5 +1,5 @@
 // CORS must be enabled it order for it work on browser (the one below works)
 // ANY url will work on server
-import("https://unpkg.com/bespoke@1.1.0/lib/bespoke.js").then(module => {
-    console.log(module);
+import("https://unpkg.com/moment@2.19.1/moment.js").then(moment => {
+    console.log(moment().format('LLLL'));
 });
