56ef110798cabf3faddb9f76b1217564a739b4bc
Ivan Orlov <nchanged@gmail.com>
2018-01-12 14:59:33 +0100

    css resource plugin

---
+++ examples/css-resource-plugin-example/fuse.js
@@ -0,0 +1,53 @@
+const { FuseBox, Sparky, WebIndexPlugin, CSSPlugin, CSSResourcePlugin, QuantumPlugin } = require("fuse-box");
+const { src, task, watch, context, fuse } = require("fuse-box/sparky");
+
+
+context(class {
+    getConfig() {
+        return FuseBox.init({
+            homeDir: "src",
+            output: "dist/$name.js",
+            hash: this.isProduction,
+            target: "browser",
+            plugins: [
+                WebIndexPlugin({
+                     template: "src/index.html"
+                }),
+
+                [CSSResourcePlugin({
+                    dist: "dist/css-resources"
+                }), CSSPlugin()],
+                this.isProduction && QuantumPlugin({
+                    bakeApiIntoBundle: "app",
+                    uglify: true,
+                    extendServerImport: true
+                })
+            ]
+        })
+    }
+    createBundle(fuse) {
+        const app = fuse.bundle("app");
+        if (!this.isProduction) {
+            app.watch()
+            app.hmr()
+        }
+        app.instructions(">index.ts");
+        return app;
+    }
+});
+
+
+task("default", async context => {
+    const fuse = context.getConfig();
+    fuse.dev();
+    context.createBundle(fuse);
+    await fuse.run();
+});
+
+task("dist", async context => {
+    context.isProduction = true;
+    const fuse = context.getConfig();
+    fuse.dev(); // remove it later
+    context.createBundle(fuse);
+    await fuse.run();
+});
